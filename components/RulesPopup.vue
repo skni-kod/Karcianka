6485093cfb940301956ed4e3530baca17aa114b1
Pawel_Piekarz <8pawelpiekarz8@gmail.com>
2021-11-14 21:34:45 +0100

    Prettier

--- components/RulesPopup.vue
+++ components/RulesPopup.vue
@@ -3,72 +3,71 @@
         <div class="rulesPopup" v-if="open" onselectstart="return false;">
             <p class="title">rulesPopup</p>
             <div class="closePopup" @click="$emit('close-rules-popup')"></div>
-        </div>  
+        </div>
     </transition>
 </template>
 
 <script>
-    export default {
-        props: ['open']
-    }
+export default {
+    props: ['open'],
+}
 </script>
 
-<style lang="scss" scoped>  
-    
-    *{
-        color: black;
-        font-family: NeonFontButtons2;
+<style lang="scss" scoped>
+* {
+    color: black;
+    font-family: NeonFontButtons2;
+}
+
+.rulesPopup {
+    z-index: 100;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(230, 230, 230);
+
+    .closePopup {
+        position: absolute;
+        width: 4vh;
+        height: 4vh;
+        top: 0.7em;
+        left: 0.7em;
+        background: url('@/assets/img/x-lg.svg');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        cursor: pointer;
     }
 
-    .rulesPopup{
-        z-index: 100;   
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgb(230, 230, 230);
+    .title {
+        width: 100%;
+        font-size: 2em;
+        text-align: center;
+    }
+}
 
-        .closePopup{
-            position: absolute;
-            width: 4vh;
-            height: 4vh;
-            top: .7em;
-            left: .7em;
-            background: url("@/assets/img/x-lg.svg");
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: 100%;
-            cursor: pointer;
-        }
-
-        .title{
-            width: 100%;
-            font-size: 2em;
-            text-align: center;
-        }
-    }
-
-    .v-enter{
-        transform: translateY(100vh);
-        opacity: 0.3;
-    }
-    .v-enter-to{
-        transform: translateY(0);
-        opacity: 1;
-    }
-    .v-enter-active{
-        transition: all .5s ease;
-    }
-    .v-leave-from{
-        transform: translateY(0);
-        opacity: 1;
-    }
-    .v-leave-to{
-        transform: translateY(100vh);
-        opacity: 0.3;
-    }
-    .v-leave-active{
-        transition: all .5s ease; 
-    }
+.v-enter {
+    transform: translateY(100vh);
+    opacity: 0.3;
+}
+.v-enter-to {
+    transform: translateY(0);
+    opacity: 1;
+}
+.v-enter-active {
+    transition: all 0.5s ease;
+}
+.v-leave-from {
+    transform: translateY(0);
+    opacity: 1;
+}
+.v-leave-to {
+    transform: translateY(100vh);
+    opacity: 0.3;
+}
+.v-leave-active {
+    transition: all 0.5s ease;
+}
 </style>
